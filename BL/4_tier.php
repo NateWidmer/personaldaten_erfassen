81b7431ef5f6d6b2d807cf0363a69797f2b001b8
Nate Widmer <nate.widmer@noseryoung.ch>
2020-09-01 11:47:13 +0200

    added sql insert statement, edited function

--- BL/4_tier.php
+++ BL/4_tier.php
@@ -1,4 +1,5 @@
 <?php
+
 /*************************************************/
 /** M151 DB 	Multi DB Connection		            **/
 /** Author: 	M. von Orelli			                **/
@@ -21,30 +22,34 @@
 /*************************************************/
 
 
- require_once("./include/function.inc"); 
- require_once("./include/db_connection.inc"); 
- 
- // $cName = "mvorelli";
- // $cPassword = "gugus1";
+require_once("./include/function.inc");
+require_once("./include/db_connection.inc");
 
-  $HTML_Out= "";
-  $cVorname = $_POST['vorname'];
-  $cNachname = $_POST['nachname'];
+// $cName = "mvorelli";
+// $cPassword = "gugus1";
 
-  echo "<font color='red'>BL Business Layer</font><br>";
+$HTML_Out = "";
+$nachname = $_POST['nachname'];
+$vorname = $_POST['vorname'];
+$geburtstag = $_POST['geburtstag'];
+$email = $_POST['email'];
+$ahv = $_POST['ahv_nr'];
+$personal = $_POST['personal_nr'];
+$telefon = $_POST['telefon'];
+$abteilung = $_POST['abteilung'];
+$berufsbez = $_POST['berufsbezeichnung'];
+$beschreibung = $_POST['beschreibung'];
 
-  $login_status = check_user_password($dbconn , $cVorname, $cNachname);
-  echo "<br><font color='red'>BL Business Layer</font>";
-  $HTML_Out= "<br><font color='red'>PL Presentation Layer</font>";
-  if ($login_status==1)
-  {
-     $HTML_Out= $HTML_Out. "<br>  Loginstatus: true Value: ";
-  }
-  else
-  {
-    $HTML_Out= $HTML_Out. "<br>  Loginstatus: false Value: ";  
-  }  
+echo "<font color='red'>BL Business Layer</font><br>";
 
-  $HTML_Out= $HTML_Out.$login_status;
-  echo  $HTML_Out;
-?>
+$login_status = register_new_user($dbconn, $vorname, $nachname, $geburtstag, $email, $ahv, $personal, $telefon, $abteilung, $berufsbez, $beschreibung);
+echo "<br><font color='red'>BL Business Layer</font>";
+$HTML_Out = "<br><font color='red'>PL Presentation Layer</font>";
+if ($login_status == 1) {
+  $HTML_Out = $HTML_Out . "<br>  Loginstatus: true Value: ";
+} else {
+  $HTML_Out = $HTML_Out . "<br>  Loginstatus: false Value: ";
+}
+
+$HTML_Out = $HTML_Out . $login_status;
+echo  $HTML_Out;
